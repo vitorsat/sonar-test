e38e9ee361d6aacc3d77a90eb92025db07e42c81
Vitor Saturnino <vitor.miguel0581@gmail.com>
2025-03-05 12:06:44 -0300

    testing sonar

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ onClick }) => {
   return (
     <button onClick={onClick} data-testid="buttonclick">
-      Click Me
+      Click Me - Testando
     </button>
   );
 };
