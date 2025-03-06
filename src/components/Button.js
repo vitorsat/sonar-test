0cac38c094af6182384bd9b3b91da1f9a088552e
Vitor Saturnino <vitor.miguel0581@gmail.com>
2025-03-06 16:22:27 -0300

    add errors

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,8 @@
 import React from "react";
 
 const Button = ({ onClick }) => {
-  const handleClick = () => {
-    eval("console.log('This is unsafe!')"); 
-  };
-
   return (
-    <button onClick={handleClick} data-testid="buttonclick">
+    <button onClick={onClick} data-testid="buttonclick">
       Click Me - Testando
     </button>
   );
